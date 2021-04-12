61828f828cd58b0cd0a4b1b7ffdf9230ab38d77e
Denzeldeveloper <denzeldeveloper@gmail.com>
2021-04-12 19:11:23 +0300

    Connected to MongoAtlas Port 5000

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import nodemon from 'nodemon';
+
+const app = express();
+
+app.use(bodyparser.json({limit: "50mb", extended: true}));
+
+app.use(bodyparser.urlencoded({limit: "50mb", extended: true}));
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://Denzel:9763@cluster0.ocgjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then(()=> app.listen(PORT,() =>
+    console.log('Connection Successful on port: ${PORT}')
+)).catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
+
+
+
