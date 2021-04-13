1ba11d2c7a7b28aa862d2a3ae7f2b427eaa560c8
Denzeldeveloper <denzeldeveloper@gmail.com>
2021-04-13 16:58:56 +0300

    FrontEnd Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,73 @@
 import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import inventory from './components/showInventory/showInventory.js';
+import Create from './components/createInventory/createInventory.js';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Denzel's HostGuest Post-Interview Solution</h1>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Container maxWidth = "lg" >
+        <
+        AppBar className = { classes.appBar }
+        position = 'static'
+        color = 'inherit' >
+        <
+        Typography className = { classes.heading }
+        variant = 'h2'
+        align = 'center' > Inventory Show < /Typography>
+
+        <
+        /AppBar>
+
+        <
+        Grow in >
+        <
+        Grid container justify = 'space-between'
+        alignItems = 'stretch' >
+        <
+        Grid item xs = { 12 }
+        sm = { 7 } >
+        <
+        AppBar className = { classes.appBar }
+        position = 'static'
+        color = 'inherit' >
+        <
+        inventory / >
+        <
+        /AppBar>
+
+        <
+        /Grid>
+
+        <
+        Grid item xs = { 12 }
+        sm = { 4 } >
+        <
+        AppBar className = { classes.appBar }
+        position = 'static'
+        color = 'inherit' >
+        <
+        Create / >
+
+        <
+        /AppBar>        
+
+        <
+        /Grid> 
+
+        <
+        /Grid>     
+
+        <
+        /Grow>
+
+        <
+        /Container> <
+        /div>
+    );
 }
 
 export default App;
---
+++ frontend/src/style.js
@@ -0,0 +1,15 @@
+import makeStyles from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+    appBar: {
+
+        borderRadius: 15,
+        margin: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }
+
+}))
